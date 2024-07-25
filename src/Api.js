17e54d878e215088ed5d22fa12a8015687bcae54
minseogo <gominseo1112@gmail.com>
2024-07-25 11:45:27 +0900

    중복된 키값에 대해서 버튼 만들기

--- src/Api.js
+++ src/Api.js
@@ -5,16 +5,18 @@ function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
 
   useEffect(() => {
     // API 호출 함수
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
         // 외부데이터 get으로 가져오고 내부데이터 post로 가져온다
         // 외부데이터 다 올때까지 대기, 전부 저장 완료후 실행
         setUsers(response.data); // setUsers은 렌더링 함수, 즉 users에 저장되고있다
         setLoading(false);
+        console.log(users);
       } catch (error) {
         setError(error);
         setLoading(false);
@@ -24,32 +26,40 @@ function App() {
     fetchUsers(); // 비동기함수 실행
   }, []); // 빈 어레이 []는 1회 실행
 
-  
-  if (error) return <p>Error: {error.message}</p>;
+  const uniqueAlbumIds = Array.from(new Set(users.map(item => item.albumId)));
+  // uniqueAlbumIds 데이터 array
+  // 클래스(붕어빵들) 생성자 new 복제 (인스턴스 : 붕어빵)
+  // 메서드 소속 클래스
 
   return (
     <div className="App">
       <h1>Users</h1>
+      <div>
+        {uniqueAlbumIds.map(albumId => (
+            <button key={albumId} value={albumId}>
+              Album {albumId}
+            </button>
+          ))
+        }
+      </div>
 
-            {/* 에러일때 노출되는 문구 */}
+      {/* 에러일때 노출되는 문구 */}
       {error && <p>Error : {error.message}</p>}
 
-     {/*로딩일때 화면에 노출되는 문구를 삼항식으로 사용*/}
-      {loading ? <p>Loading...</p> : 
-
-        <ul>
-            {users.map(user => (
-            <li key={user.id}>
-                <p>이름 : {user.name}</p>
-                <p>이메일 : {user.email}</p>
-                <p>주소 : {`${user.address["street"]} ${user.address["suite"]}`}</p>
+      {/* 로딩일때 화면에 노출되는 문구를 삼항식으로 사용 */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul style={{display: "flex", flexWrap: "wrap"}}>
+          {users.filter(item => item.albumId === 1).map(user => (
+            <li key={user.id} style={{width: "33%", listStyle: "none", padding: "0"}}>
+              <h2 style={{fontSize: "16px"}}>{user.title}</h2>
+              <img src={user.thumbnailUrl} alt={user.title} />
             </li>
-            ))}
+          ))}
         </ul>
-      }
-
+      )}
     </div>
   );
 }
-
 export default App;
